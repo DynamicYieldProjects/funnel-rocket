531d5cd4c62a9d6be6241cb10619703e989ede94
Elad Rosenheim <elad@dynamicyield.com>
2021-02-14 12:04:47 +0200

    Complete API server tests; tweaking docker build

--- tests/test_apiserver.py
+++ tests/test_apiserver.py
@@ -1,12 +1,16 @@
 import json
 import os
+import time
 from dataclasses import dataclass
 from enum import auto
 from typing import Optional, Union
 import requests
 from frocket.common.helpers.utils import timestamped_uuid
 from frocket.common.serializable import AutoNamedEnum
-from tests.utils.dataset_utils import new_test_dataset, DEFAULT_GROUP_COLUMN, DEFAULT_TIMESTAMP_COLUMN
+from tests.utils.dataset_utils import new_test_dataset, DEFAULT_GROUP_COLUMN, DEFAULT_TIMESTAMP_COLUMN, TIME_SHIFT, \
+    BASE_TIME, TestColumn, BASE_USER_ID, DEFAULT_GROUP_COUNT, datafile_schema
+
+# TODO as part of automated failure/retry tests: test query timeout
 
 TEST_API_SERVER_URL = os.environ.get('TEST_API_SERVER_URL', 'http://127.0.0.1:5000/')
 if not TEST_API_SERVER_URL.endswith('/'):
@@ -25,6 +29,7 @@ class UrlMethod(AutoNamedEnum):
 class UrlInfo:
     url: str
     method: UrlMethod
+    returns_success_key: bool = True
 
 
 @dataclass
@@ -35,37 +40,53 @@ class ResponseInfo:
 
 class URLs:
     @staticmethod
-    def register_url_info(run_async: bool = False) -> UrlInfo:
+    def register_url(run_async: bool = False) -> UrlInfo:
         return UrlInfo(
             url=f"{TEST_API_SERVER_URL}datasets/register?stream={str(run_async).lower()}",
             method=UrlMethod.POST)
 
     @staticmethod
-    def unregister_url_info(ds_name: str):
+    def unregister_url(ds_name: str, force: bool = False) -> UrlInfo:
         return UrlInfo(
-            url=f"{TEST_API_SERVER_URL}datasets/{ds_name}/unregister",
+            url=f"{TEST_API_SERVER_URL}datasets/{ds_name}/unregister?force={str(force).lower()}",
             method=UrlMethod.POST)
 
     @staticmethod
-    def list_datasets_url_info():
-        return UrlInfo(
-            url=f"{TEST_API_SERVER_URL}datasets",
-            method=UrlMethod.GET)
+    def list_datasets_url() -> UrlInfo:
+        return UrlInfo(url=f"{TEST_API_SERVER_URL}datasets", method=UrlMethod.GET,
+                       returns_success_key=False)
 
     @staticmethod
-    def run(info: UrlInfo,
+    def dataset_parts_url(ds_name: str) -> UrlInfo:
+        return UrlInfo(url=f"{TEST_API_SERVER_URL}datasets/{ds_name}/parts", method=UrlMethod.GET,
+                       returns_success_key=False)
+
+    @staticmethod
+    def dataset_schema_url(ds_name: str, full: bool = True) -> UrlInfo:
+        return UrlInfo(
+            url=f"{TEST_API_SERVER_URL}datasets/{ds_name}/schema?full={str(full).lower()}",
+            method=UrlMethod.GET, returns_success_key=False)
+
+    @staticmethod
+    def query_url(ds_name: str, run_async: bool = False) -> UrlInfo:
+        return UrlInfo(
+            url=f"{TEST_API_SERVER_URL}datasets/{ds_name}/query?stream={str(run_async).lower()}",
+            method=UrlMethod.POST)
+
+    @staticmethod
+    def run(url_info: UrlInfo,
             json_body: dict = None,
             expected_success: bool = True,
             expect_stream: bool = False,
             errors_are_json: bool = True,
             expected_dict: dict = None,
             stream_expected_task_count: int = None) -> ResponseInfo:
-        print(f"UrlInfo: {info}, json_body: {json_body}, expected success: {expected_success}, "
-              f"expected dict: {expected_dict}")
-        if info.method == UrlMethod.POST:
-            response = requests.post(info.url, json=json_body, stream=expect_stream)
-        elif info.method == UrlMethod.GET:
-            response = requests.get(info.url, stream=expect_stream)
+        # print(f"UrlInfo: {url_info}, json_body: {json_body}, expected success: {expected_success}, "
+        #       f"expected dict: {expected_dict}")
+        if url_info.method == UrlMethod.POST:
+            response = requests.post(url_info.url, json=json_body, stream=expect_stream)
+        elif url_info.method == UrlMethod.GET:
+            response = requests.get(url_info.url, stream=expect_stream)
         else:
             raise Exception('Uknown method!')
 
@@ -87,18 +108,20 @@ class URLs:
         else:
             response_json = None
 
-        print(f"Response as text: {response_text}")
+        # print(f"Response as text: {response_text}")
         if expected_success:
             assert response.status_code == 200
             assert type(response_json) in [dict, list]
-            if type(response_json) is dict:
+            if url_info.returns_success_key:
                 assert response_json['success'] is True
                 assert not response_json.get('errorMessage', None)
         else:
+            print(f"Response as text: {response_text}")
             assert response.status_code == 500 if not expect_stream else 200
             if errors_are_json:
-                assert response_json['success'] is False
-                assert response_json['errorMessage'] is not None
+                if url_info.returns_success_key:
+                    assert response_json['success'] is False
+                    assert response_json['errorMessage'] is not None
             else:
                 assert response_text.startswith(ERROR_STRING_PREFIX)
 
@@ -109,96 +132,65 @@ class URLs:
         return ResponseInfo(response_text, response_json)
 
 
+def build_register_args(basepath: str, ds_name: str = None):
+    ds_name = ds_name or timestamped_uuid(prefix="ds-")
+    return {
+        "name": ds_name,
+        "basepath": basepath,
+        "group_id_column": DEFAULT_GROUP_COLUMN,
+        "timestamp_column": DEFAULT_TIMESTAMP_COLUMN,
+        "validation_mode": "FIRST_LAST",
+        "validate_uniques": True
+    }
+
+
 def test_register_unregister_ok():
     with new_test_dataset(4) as test_ds:
-        s3path = test_ds.copy_to_s3()
-        ds_name = timestamped_uuid(prefix="ds-")
-        body = {
-            "name": ds_name,
-            "basepath": s3path,
-            "group_id_column": DEFAULT_GROUP_COLUMN,
-            "timestamp_column": DEFAULT_TIMESTAMP_COLUMN,
-            "validation_mode": "FIRST_LAST",
-            "validate_uniques": True
-        }
-        URLs.run(info=URLs.register_url_info(run_async=False), json_body=body)
-        URLs.run(info=URLs.unregister_url_info(ds_name=ds_name), expected_dict={'datasetFound': True})
-        URLs.run(info=URLs.unregister_url_info(ds_name=ds_name), expected_dict={'datasetFound': False})
+        args = build_register_args(basepath=test_ds.copy_to_s3())
+        ds_name = args['name']
+        URLs.run(url_info=URLs.register_url(run_async=False), json_body=args)
+        URLs.run(url_info=URLs.unregister_url(ds_name=ds_name), expected_dict={'datasetFound': True})
+        URLs.run(url_info=URLs.unregister_url(ds_name=ds_name), expected_dict={'datasetFound': False})
 
 
 def test_register_invalid_path():
-    body = {
-        "name": "ds-invalid-path",
-        "basepath": "s3://thisisnot/apath/",
-        "group_id_column": DEFAULT_GROUP_COLUMN,
-        "timestamp_column": DEFAULT_TIMESTAMP_COLUMN,
-        "validation_mode": "SINGLE",
-        "validate_uniques": False
-    }
-    URLs.run(info=URLs.register_url_info(run_async=False), json_body=body,
+    args = build_register_args(basepath="s3://thisisnot/apath/")
+    URLs.run(url_info=URLs.register_url(run_async=False), json_body=args,
              expected_success=False, errors_are_json=True)
 
 
 def test_register_invalid_column():
     with new_test_dataset(1) as test_ds:
-        s3path = test_ds.copy_to_s3()
-        body = {
-            "name": "ds-invalid-column",
-            "basepath": s3path,
-            "group_id_column": "notagroupidcolumn",
-            "timestamp_column": DEFAULT_TIMESTAMP_COLUMN,
-            "validation_mode": "SINGLE",
-            "validate_uniques": False
-        }
-        URLs.run(info=URLs.register_url_info(run_async=False), json_body=body,
+        args = build_register_args(basepath=test_ds.copy_to_s3())
+        args['group_id_column'] = 'notagroupidcolumn'
+        URLs.run(url_info=URLs.register_url(run_async=False), json_body=args,
                  expected_success=False, errors_are_json=True)
 
 
 def test_register_invalid_args():
     with new_test_dataset(1) as test_ds:
         s3path = test_ds.copy_to_s3()
-        body = {
-            "name": "ds-invalid-args",
-            "basepath": s3path,
-            "group_id_column": DEFAULT_GROUP_COLUMN,
-            "timestamp_column": DEFAULT_TIMESTAMP_COLUMN,
-            "validation_mode": "SINGLEX",  # Invalid
-            "validate_uniques": True
-        }
-        URLs.run(info=URLs.register_url_info(run_async=False), json_body=body,
+        args = build_register_args(s3path)
+        args['validation_mode'] = 'SINGLEX'  # Invalid
+        URLs.run(url_info=URLs.register_url(run_async=False), json_body=args,
                  expected_success=False, errors_are_json=False)
 
-        body = {
-            "name": "ds-invalid-args",
-            "basepath": s3path,
-            "group_id_column": DEFAULT_GROUP_COLUMN,
-            "timestamp_column": DEFAULT_TIMESTAMP_COLUMN,
-            "validation_mode": "SINGLE",
-            "validate_uniques": 123  # Invalid
-        }
-        URLs.run(info=URLs.register_url_info(run_async=False), json_body=body,
+        args = build_register_args(s3path)
+        args['validate_uniques'] = '123'  # Invalid
+        URLs.run(url_info=URLs.register_url(run_async=False), json_body=args,
                  expected_success=False, errors_are_json=False)
 
 
 def test_register_async():
     with new_test_dataset(2) as test_ds:
-        s3path = test_ds.copy_to_s3()
-        ds_name = timestamped_uuid(prefix="ds-")
-        body = {
-            "name": ds_name,
-            "basepath": s3path,
-            "group_id_column": DEFAULT_GROUP_COLUMN,
-            "timestamp_column": DEFAULT_TIMESTAMP_COLUMN,
-            "validation_mode": "FIRST_LAST",
-            "validate_uniques": True
-        }
-        URLs.run(info=URLs.register_url_info(run_async=True), json_body=body,
+        args = build_register_args(test_ds.copy_to_s3())
+        URLs.run(url_info=URLs.register_url(run_async=True), json_body=args,
                  expect_stream=True, stream_expected_task_count=2)
-        URLs.run(info=URLs.unregister_url_info(ds_name=ds_name), expected_dict={'datasetFound': True})
+        URLs.run(url_info=URLs.unregister_url(ds_name=args['name']), expected_dict={'datasetFound': True})
 
 
 def test_list_datasets():
-    ds_list = URLs.run(info=URLs.list_datasets_url_info()).json
+    ds_list = URLs.run(url_info=URLs.list_datasets_url()).json
     assert type(ds_list) is list
     for e in ds_list:
         assert sorted(e.keys()) == LIST_DATASETS_EXPECTED_KEYS
@@ -206,48 +198,133 @@ def test_list_datasets():
 
     num_parts = 4
     with new_test_dataset(num_parts) as test_ds:
-        ds_name = timestamped_uuid('findmehere-')
-        s3path = test_ds.copy_to_s3()
-        body = {
-            "name": ds_name,
-            "basepath": s3path,
-            "group_id_column": DEFAULT_GROUP_COLUMN,
-            "timestamp_column": DEFAULT_TIMESTAMP_COLUMN,
-            "validation_mode": "SINGLE",
-            "validate_uniques": False
-        }
-        URLs.run(info=URLs.register_url_info(), json_body=body)
+        args = build_register_args(test_ds.copy_to_s3())
+        ds_name = args['name']
+        URLs.run(url_info=URLs.register_url(), json_body=args)
 
-    updated_list = URLs.run(info=URLs.list_datasets_url_info()).json
+    updated_list = URLs.run(url_info=URLs.list_datasets_url()).json
     assert len(updated_list) == original_count + 1
     found = [e for e in updated_list if e['id']['name'] == ds_name]
     assert len(found) == 1
-    assert found[0]['basepath'] == s3path and found[0]['totalParts'] == num_parts
+    assert found[0]['basepath'] == args['basepath'] and found[0]['totalParts'] == num_parts
 
-    URLs.run(URLs.unregister_url_info(ds_name))
-    updated_list = URLs.run(info=URLs.list_datasets_url_info()).json
+    URLs.run(URLs.unregister_url(ds_name))
+    updated_list = URLs.run(url_info=URLs.list_datasets_url()).json
     assert len(updated_list) == original_count
 
 
 def test_unregister_force():
-    pass#TODO
+    with new_test_dataset(1) as test_ds:
+        args = build_register_args(test_ds.copy_to_s3())
+        ds_name = args['name']
+        URLs.run(url_info=URLs.register_url(), json_body=args)
+        curr_second = int(time.time())
+        URLs.run(url_info=URLs.query_url(ds_name), json_body={})
 
+        response = URLs.run(url_info=URLs.unregister_url(ds_name=args['name'], force=False),
+                            expected_success=False, errors_are_json=True)
+        assert curr_second <= response.json['datasetLastUsed'] < curr_second + 10
+        assert 'less than safety interval' in response.json['errorMessage']
 
-def test_dataset_details():
-    pass#TODO get dataset, schema - full & short, parts
+        URLs.run(url_info=URLs.unregister_url(ds_name=args['name'], force=True),
+                 expected_success=True, expected_dict={'datasetFound': True})
+        URLs.run(url_info=URLs.unregister_url(ds_name=args['name'], force=False),
+                 expected_success=True, expected_dict={'datasetFound': False})
 
 
 def test_query_ok():
-    pass#TODO sync, async
+    num_parts = 3
+    with new_test_dataset(num_parts) as test_ds:
+        args = build_register_args(test_ds.copy_to_s3())
+        ds_name = args['name']
+        URLs.run(url_info=URLs.register_url(), json_body=args)
+
+        query = {
+            'query': {
+                'conditions': [
+                    # Should exclude exactly one user (one with lowest ID)
+                    {'filter': [DEFAULT_GROUP_COLUMN, '>', BASE_USER_ID]}
+                ]
+            },
+            'funnel': {
+                'sequence': [
+                    # Should include exactly one user (one with higehst ID)
+                    {'filter': [DEFAULT_TIMESTAMP_COLUMN, '>=', BASE_TIME + (TIME_SHIFT * num_parts)]}
+                ],
+                "endAggregations": [
+                    {'column': TestColumn.bool, 'type': 'countPerValue'}
+                ]
+            }
+        }
+        response = URLs.run(url_info=URLs.query_url(ds_name), json_body=query)
+        assert response.json['query']['matchingGroups'] == (DEFAULT_GROUP_COUNT * 2) - 1
+        assert response.json['funnel']['sequence'][0]['matchingGroups'] == 1
+        assert sum(response.json['funnel']['endAggregations'][0]['value'].values()) == \
+               response.json['funnel']['sequence'][0]['matchingGroupRows']
+
+        URLs.run(url_info=URLs.unregister_url(ds_name=args['name'], force=True),
+                 expected_success=True, expected_dict={'datasetFound': True})
 
 
 def test_query_invalid_args():
-    pass#TODO invalid dataset name, invalid schema
+    """Errors that should be raise already on invoker side, and result in 'Error: ...' string response"""
+    with new_test_dataset(1) as test_ds:
+        args = build_register_args(test_ds.copy_to_s3())
+        ds_name = args['name']
+        URLs.run(url_info=URLs.register_url(), json_body=args)
+
+        response = URLs.run(url_info=URLs.query_url(ds_name), json_body={'blah': 123},
+                            expected_success=False, errors_are_json=False)
+        assert "'blah' was unexpected" in response.text
+
+        invalid_ds_name = ds_name + 'blah!!!'
+        response = URLs.run(url_info=URLs.query_url(invalid_ds_name), json_body={},
+                            expected_success=False, errors_are_json=False)
+        assert f"Dataset {invalid_ds_name} not found" in response.text
+
+        URLs.run(url_info=URLs.unregister_url(ds_name, force=True))
 
 
 def test_query_missing_files():
-    pass#TODO
+    """Missing files (after registration) should cause workers to fail"""
+    with new_test_dataset(2) as test_ds:
+        args = build_register_args(test_ds.copy_to_s3())
+        args['validation_mode'] = 'SINGLE'
+        ds_name = args['name']
+
+        # Query works, initially
+        URLs.run(url_info=URLs.register_url(), json_body=args)
+        URLs.run(url_info=URLs.query_url(ds_name), json_body={})
+
+        # Now re-register; files already cached should be disregarded,
+        # and register itself is set to only validate a SINGLE file (that would be cached).
+        # Meaning, other parts should not be locally cached, and loading from source should now fail
+        URLs.run(url_info=URLs.register_url(), json_body=args)
+        for key in test_ds.bucket.objects.all():
+            key.delete()
+
+        # Expected to fail "as JSON" (with a status and errorMessage), as it's errors from workers
+        response = URLs.run(url_info=URLs.query_url(ds_name), json_body={},
+                            expected_success=False, errors_are_json=True)
+
+        URLs.run(url_info=URLs.unregister_url(ds_name, force=True))
 
 
-# TODO no raw metrics (and test only expected keys are returned)
-# TODO query timeout tests...
+def test_dataset_details():
+    """A non-exhaustive test of fetching dataset parts and schema info (this was tested in detail elsewhere)"""
+    with new_test_dataset(2) as test_ds:
+        args = build_register_args(test_ds.copy_to_s3())
+        ds_name = args['name']
+        URLs.run(url_info=URLs.register_url(), json_body=args)
+
+        ds_parts = URLs.run(url_info=URLs.dataset_parts_url(ds_name)).json
+        assert ds_parts['filenames'] == test_ds.expected_parts.filenames
+        expected_columns = json.loads(datafile_schema().to_json())['columns']
+
+        ds_short_schema = URLs.run(url_info=URLs.dataset_schema_url(ds_name, full=False)).json
+        assert ds_short_schema['columns'] == expected_columns
+
+        ds_full_schema = URLs.run(url_info=URLs.dataset_schema_url(ds_name, full=True)).json
+        assert ds_full_schema['columns'][DEFAULT_TIMESTAMP_COLUMN]['colattrs']['numericMin'] == BASE_TIME
+
+        URLs.run(url_info=URLs.unregister_url(ds_name))
